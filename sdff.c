7efb37f7455fd10a198123f69a89e4106a2d0b3e
adrita99 <34001996+adrita99@users.noreply.github.com>
2017-11-26 04:42:10 -0800

    Add files via upload

---
+++ sdff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int T, a,b,sum,i;
+    scanf("%d",&T);
+    for(i=1;i<=T;i++)
+    {
+        scanf("%d%d",&a,&b);
+        sum=a+b;
+        printf("Case %d: %d\n",i,sum);
+    }
+    return 0;
+
+}
